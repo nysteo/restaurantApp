220a1d46d93a44a3ce1ca3487e8d3e01050e293d
nysteo <teonys@nyu.edu>
2020-08-12 17:01:09 -0400

    Creating Zomato API request

---
+++ src/App.jsx
@@ -0,0 +1,121 @@
+import React, {useEffect, useState} from 'react';
+import {ThemeProvider, createMuiTheme, responsiveFontSizes, Typography, Grid, Fade, Button, makeStyles, Box} from '@material-ui/core'
+import logo from './logo.svg';
+import './App.css';
+
+const contrastText = '#2C3C56';
+let theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#306DDF',
+            contrastText: '#ffff',
+        },
+        secondary: {
+            main: '#FEAD18',
+            contrastText: contrastText,
+        },
+        error: {
+            main: '#F46D66',
+            contrastText: contrastText,
+        },
+        info: {
+            main: '#7064D0',
+            contrastText: contrastText,
+        },
+        success: {
+            main: '#47C594',
+            contrastText: contrastText,
+        },
+        background: '#F5F8FF',
+        text: {
+            primary: '#2C3C56',
+            secondary: '#AEAEAE',
+        },
+    },
+    typography: {
+        fontFamily: 'Poppins',
+    },
+    overrides: {
+        MuiTab: {
+            wrapper: {
+                flexDirection: 'row',
+            },
+        },
+    },
+});
+theme = responsiveFontSizes(theme);
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    background: '#EBEFF2',
+    height: '100vh',
+  },
+  greetingsContainer: {
+    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+    background: '#306CDF',
+    padding: '1rem 3rem',
+    borderRadius: '4px',
+    color: '#fff',
+  }
+  
+}));
+
+
+
+const App = (props) => {
+  const apiLink = 'https://developers.zomato.com/api/v2.1/';
+  const classes = useStyles();
+  const [userLat, setLat] = useState(40.7273472);
+  const [userLon, setLon] = useState(-73.8492416);
+  const [userLocation, setUserLocation] = useState('unknown');
+  const [nearbyRestaurants, setNearbyRestaurants] = useState([]);
+
+ 
+    
+
+    
+  
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+    fetch(`${apiLink}/geocode?lat=${userLat}&lon=${userLon}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'user-key': '6a14e6f5ec6a7ff2475989f5ba2b27e9',
+        }})
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res)
+          setUserLocation(res.location.title);
+          setNearbyRestaurants(res.nearby_restaurants);
+        },
+        (error) => {
+            console.log(error);
+        });
+  }, [apiLink]);
+
+  return (
+    <Fade in timeout={1000}>
+      <div className = {classes.container}>
+        <Grid container direction = 'row' spacing = {0} justify = 'center' alignItems = 'stretch' alignContent = 'strecth'>
+          <Grid item xs={12} md={6} lg={7} xl={7} style={{marginLeft: '1rem', marginTop: '1rem', height: '80vmin'}}>
+            <Grid container direction='column' spacing={2}>
+                <Grid item>
+                    <div className={classes.greetingsContainer}>
+                        <Typography variant='h6'><Box fontWeight='bold'>Restaurant Finder App</Box></Typography>
+                        <Typography variant='subtitle2' style={{opacity: '0.7'}}>Current Location: {userLocation} </Typography>
+                    </div>
+                </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </Fade>
+
+  );
+}
+
+export default App;
---
+++ src/components/RestaurantItem.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+import {Grid, Typography, makeStyles, Button, Box, Link} from '@material-ui/core';
